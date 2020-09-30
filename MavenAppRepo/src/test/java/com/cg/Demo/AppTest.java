b606bbca907bd81bb3a8b1436b7bf979bab5e815
Kotikumar <kotikumaramanikanta@gmail.com>
2020-09-30 12:10:22 +0530

    updated one in apptest

--- MavenAppRepo/src/test/java/com/cg/Demo/AppTest.java
+++ MavenAppRepo/src/test/java/com/cg/Demo/AppTest.java
@@ -1,10 +1,24 @@
 package com.cg.Demo;
 
-public class AppTest {
+import org.junit.Test;
 
-	public static void main(String[] args) {
+public class AppTest {
 		
-		
-	}
+		@Test
+		public void test1() {
+		System.out.println("test1");
+		}
+		@Test
+		public void test2() {
+		System.out.println("test2");
+		}
+		@Test
+		public void test3() {
+		System.out.println("test3");
+		}
+		@Test
+		public void test4() {
+		System.out.println("test4");
+		}
 
 }
